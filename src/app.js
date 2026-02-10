9efb11c66e2f1191a6e4e955ddc203b24f3b7d11
Manaswi Kotwal <manaswikotwal04@gmail.com>
2026-02-10 16:03:24 +0530

    explore rotes and http methods

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express=require('express');
 const app=express();
-app.use("/",(req,res) =>{
-    res.send("Hello World from dashbpard");
+
+
+app.get("/user",(req,res) => {
+    res.send({firstname:"John",lastname:"Doe"});
+});
+app.post("/user",(req,res) => {
+    console.log(req.body);
+    res.send("User created successfully");
 });
 app.use("/Hello",(req,res) => {
     res.send("Hello from Hello route");
 });
 app.use("/test", (req,res) => {
     res.send("Hello from server");
+});
+app.use("/",(req,res) =>{
+    res.send("Hello World from dashbpard");
 });
 app.listen(3000,() =>{
     console.log("Server is running on port 3000");
